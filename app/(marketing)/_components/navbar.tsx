950ef8f85f18fbe4c9087d25f050597088dc4e9c
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-02 15:14:58 +0300

    feat(ui): update style navbar.

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,9 +3,7 @@
 import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
-import { Menu } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { Menu, Search } from "lucide-react";
 
 import {
   NavigationMenu,
@@ -23,44 +21,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+import { Input } from "@/components/ui/input";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
@@ -90,8 +51,8 @@ export const Navbar = () => {
           </SheetHeader>
         </SheetContent>
       </Sheet>
-      <div className="flex justify-start items-center h-20 gap-x-[100px] max-w-screen-xl mx-auto px-4 md:px-8">
-        <Link href="/">
+      <NavigationMenu>
+        <Link href="/" className="h-full flex justify-center items-center">
           <Image
             src="/logo-light.svg"
             alt="E-In-Akademie Logo Light"
@@ -100,48 +61,44 @@ export const Navbar = () => {
             priority
           />
         </Link>
-        <NavigationMenu className="hidden lg:flex">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Categories</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  In Akademie Business
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div className="ml-auto hidden lg:flex justify-center items-center gap-3">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Categories</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="min-h-[73px] flex justify-start items-center flex-wrap gap-x-8 gap-y-4 py-5">
+                {[
+                  "E-CHECK",
+                  "TETRA",
+                  "ELECTROMOBILITY",
+                  "TREI",
+                  "SAFETY TECHNOLOGY",
+                  "TECHNOLOGY",
+                  "STANDARDS & OCCUPATIONAL SAFETY",
+                  "ONLINE SEMINARS",
+                ].map((item, index) => (
+                  <Button
+                    key={index}
+                    size="sm"
+                    variant="ghost"
+                    className="text-[#656565]"
+                  >
+                    {item}
+                  </Button>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                In Akademie Business
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        <div className="h-full ml-auto hidden lg:flex justify-center items-center gap-3">
           <div className="flex justify-center items-center gap-2">
-            <Button asChild size="icon" variant="outline">
-              <Link href="/">
-                <Image
-                  src="/search.svg"
-                  alt="Search Icon"
-                  width={20}
-                  height={20}
-                  priority
-                  className="object-contain"
-                />
-                <span className="sr-only">Search Icon</span>
-              </Link>
-            </Button>
+            <Input placeholder="Search.." icon={Search} />
             <Button asChild size="icon" variant="outline">
               <Link href="/">
                 <Image
@@ -162,42 +119,13 @@ export const Navbar = () => {
             </Link>
           </Button>
         </div>
-        <Button
-          variant="outline"
-          size="icon"
-          className="inline-flex lg:hidden ml-auto"
-          onClick={() => setIsOpen(true)}
-        >
-          <Menu className="h-5 w-5" />
-          <span className="sr-only">Menu Icon</span>
-        </Button>
-      </div>
+        <div className="h-full ml-auto inline-flex lg:hidden justify-center items-center">
+          <Button variant="outline" size="icon" onClick={() => setIsOpen(true)}>
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Menu Icon</span>
+          </Button>
+        </div>
+      </NavigationMenu>
     </>
   );
 };
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -11,13 +11,12 @@ const NavigationMenu = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <NavigationMenuPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative z-10 flex max-w-max flex-1 items-center justify-center",
-      className
-    )}
+    className={cn("relative flex-1", className)}
     {...props}
   >
-    {children}
+    <div className="h-[86px] max-w-screen-xl w-full mx-auto px-4 md:px-8 flex justify-between gap-[100px]">
+      {children}
+    </div>
     <NavigationMenuViewport />
   </NavigationMenuPrimitive.Root>
 ));
@@ -30,7 +29,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "group flex flex-1 list-none items-center justify-center space-x-1",
+      "group h-full flex-1 hidden lg:flex list-none justify-center gap-10",
       className
     )}
     {...props}
@@ -41,7 +40,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-[3px] bg-background px-4 py-2 text-base font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "group inline-flex h-full w-max items-center justify-center text-base font-medium transition-colors border-b-2 border-transparent hover:border-[#FDC511] border-opacity-0 hover:border-opacity-100 hover:text-[#FDC511] focus:[#FDC511] focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 duration-300 ease-in-out"
 );
 
 const NavigationMenuTrigger = React.forwardRef<
@@ -55,7 +54,7 @@ const NavigationMenuTrigger = React.forwardRef<
   >
     {children}{" "}
     <ChevronDown
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+      className="relative top-[1px] ml-1 h-3 w-3 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
   </NavigationMenuPrimitive.Trigger>
@@ -69,7 +68,7 @@ const NavigationMenuContent = React.forwardRef<
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={cn(
-      "left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ",
+      "left-0 top-0 !w-full px-4 lg:px-6 xl:px-28 data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
       className
     )}
     {...props}
@@ -83,10 +82,10 @@ const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-  <div className={cn("absolute left-0 top-full flex justify-center")}>
+  <div className={cn("absolute top-full !w-full flex justify-center")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
-        "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
+        "origin-top-center relative h-[var(--radix-navigation-menu-viewport-height)] !w-full overflow-hidden border-y bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
         className
       )}
       ref={ref}
