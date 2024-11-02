2877ca60ae03c4626ceb05bc75fc6567ecc3d87a
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-02 10:27:42 +0300

    feat(ui): update style hero component.

--- app/(marketing)/(routes)/(home)/page.tsx
+++ app/(marketing)/(routes)/(home)/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "./_components/hero";
 
 export default function MarketingPage() {
   return (
-    <main className="max-w-screen-xl mx-auto px-4 md:px-8">
+    <main>
       <Hero />
     </main>
   );
