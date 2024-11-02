b083f25038296c3d998364657882cd0094c5fdf8
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-02 18:05:27 +0300

    feat(ui): create CTA component.

---
+++ app/(marketing)/(routes)/(home)/_components/cta.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+export const CTA = () => {
+  return (
+    <section className="py-20">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8 flex flex-col justify-center items-center gap-8">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-medium text-center max-w-[636px] leading-10 md:leading-[55px] lg:leading-[61.6px]">
+          Qualify your team within the in-Akademie{" "}
+          <span className="text-[#FDC511]">Business</span>
+        </h2>
+        <p className="text-lg font-normal leading-7 text-[#656565] text-center max-w-[845px]">
+          Elevate your team&apos;s expertise through in-Akademie Business
+          qualification, unlocking new levels of success and professional
+          growth.
+        </p>
+        <Button className="w-full sm:w-fit px-10 bg-[#FDC511] hover:bg-[#FDC511]/90 drop-shadow">
+          Einstieg in die Akademie Business
+        </Button>
+      </div>
+    </section>
+  );
+};
--- app/(marketing)/(routes)/(home)/page.tsx
+++ app/(marketing)/(routes)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Courses } from "./_components/courses";
 import { Categories } from "./_components/categories";
 import { ExperiencedInstructor } from "./_components/experienced-instructor";
 import { Seminars } from "./_components/seminars";
+import { CTA } from "./_components/cta";
 
 export default function MarketingPage() {
   return (
@@ -12,6 +13,7 @@ export default function MarketingPage() {
       <Categories />
       <ExperiencedInstructor />
       <Seminars />
+      <CTA />
     </main>
   );
 }
