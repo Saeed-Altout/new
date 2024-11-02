b97e3bee99984f1317b7d1930f7b230cb35342b3
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-02 21:48:00 +0300

    feat(ui): create register component.

--- Schemas/index.ts
+++ Schemas/index.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmed_password: z.string().min(8),
+});
