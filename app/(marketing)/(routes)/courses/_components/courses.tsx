95182701f875fc839d6b59b8f317d2fd24a266db
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-07 12:40:20 +0300

    refactor(ui): update data courses page.

--- app/(marketing)/(routes)/courses/_components/courses.tsx
+++ app/(marketing)/(routes)/courses/_components/courses.tsx
@@ -1,7 +1,7 @@
 import { Heading } from "@/components/ui/heading";
 import { CourseCard } from "@/components/cards/course-card";
 
-import { searchCourses } from "@/constants";
+import { allCourses } from "@/constants";
 
 export const Courses = () => {
   return (
@@ -9,7 +9,7 @@ export const Courses = () => {
       <div className="max-w-screen-xl mx-auto px-4 md:px-8">
         <Heading title="Our popular seminars" label="Overview" />
         <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {searchCourses.map((course, index) => (
+          {allCourses.map((course, index) => (
             <CourseCard initialData={course} key={index} />
           ))}
         </div>
--- constants/index.ts
+++ constants/index.ts
@@ -357,3 +357,137 @@ export const searchCourses = [
     href: "/",
   },
 ];
+export const allCourses = [
+  {
+    id: 0,
+    title: "Azubi-Coaching zur perfekten Prüfungsvorbereitung in Kulmbach..",
+    cover: "/course-01.png",
+    categories: [
+      {
+        id: 0,
+        label: "Azubi",
+      },
+      {
+        id: 1,
+        label: "Kulmbach",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "250",
+    currency: "EUR",
+    href: "/",
+  },
+  {
+    id: 1,
+    title:
+      "Vorbereitungskurs Elektroniker Gesellenprüfung Teil 1 (GP 1) PRAXIS",
+    cover: "/course-02.png",
+    categories: [
+      {
+        id: 0,
+        label: "Azubi",
+      },
+      {
+        id: 1,
+        label: "Coburg",
+      },
+      {
+        id: 2,
+        label: "Kronach",
+      },
+      {
+        id: 3,
+        label: "Kulmbach",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "180",
+    currency: "EUR",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "The mechanic as a trainee mentor in Kulmbach from July 28th, 2023",
+    cover: "/course-03.png",
+    categories: [
+      {
+        id: 0,
+        label: "trainee",
+      },
+      {
+        id: 1,
+        label: "Kulmbach",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "23.7",
+    currency: "EUR",
+    href: "/",
+  },
+  {
+    id: 3,
+    title: "Electromobility Specialist (ZVEH)",
+    cover: "/seminar-01.png",
+    categories: [
+      {
+        id: 0,
+        label: "trainee",
+      },
+      {
+        id: 1,
+        label: "TREI expertise",
+      },
+      {
+        id: 2,
+        label: "certification",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "83.4",
+    currency: "EUR",
+    href: "/",
+  },
+  {
+    id: 4,
+    title:
+      "Blitz- und Überspannungsschutz Aufbauseminar “Regenerative Techniken und Elektromobilität“ am 07.11.2023",
+    cover: "/seminar-02.png",
+    categories: [
+      {
+        id: 0,
+        label: "trainee",
+      },
+      {
+        id: 1,
+        label: "Kulmbach",
+      },
+      {
+        id: 2,
+        label: "online seminar",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "45.8",
+    currency: "EUR",
+    href: "/",
+  },
+  {
+    id: 5,
+    title: "Entry coaching for trainees for the... ",
+    cover: "/seminar-03.png",
+    categories: [
+      {
+        id: 0,
+        label: "trainee",
+      },
+      {
+        id: 1,
+        label: "Kulmbach",
+      },
+    ],
+    owner: "Yannis Yannkopolus",
+    price: "36.9",
+    currency: "EUR",
+    href: "/",
+  },
+];
