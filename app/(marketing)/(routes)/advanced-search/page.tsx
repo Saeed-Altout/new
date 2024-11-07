9d4c9ce0abfbc8d954ded9c18e4b6e5242d9cff0
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-07 12:38:10 +0300

    refactor(ui): update name advanced search page.

--- app/(marketing)/(routes)/advanced-search/page.tsx
+++ app/(marketing)/(routes)/advanced-search/page.tsx
@@ -1,7 +1,7 @@
 import { Courses } from "./_components/courses";
 import { FilterForm } from "./_components/filter-form";
 
-export default function AdvancedSearch() {
+export default function AdvancedSearchPage() {
   return (
     <main className="min-h-full overflow-x-hidden">
       <FilterForm />
