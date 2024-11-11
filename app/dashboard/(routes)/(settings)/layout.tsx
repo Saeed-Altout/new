4fd74aff4233fdf1665efa55a4626bc105494207
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-11 11:23:40 +0300

    feat(ui): edit style and layout dashboard.

--- app/dashboard/(routes)/(settings)/layout.tsx
+++ app/dashboard/(routes)/(settings)/layout.tsx
@@ -6,9 +6,11 @@ export default function SettingsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-full flex flex-row">
+    <div className="h-[calc(100vh-86px)] flex flex-row">
       <Sidebar />
-      <main className="flex-1 p-16">{children}</main>
+      <div className="min-h-full flex-1 overflow-y-auto px-16 py-10 [&::-webkit-scrollbar]:w-[5px] [&::-webkit-scrollbar-thumb]:bg-[#FDC511]">
+        {children}
+      </div>
     </div>
   );
 }
