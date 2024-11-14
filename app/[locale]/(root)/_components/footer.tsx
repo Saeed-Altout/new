7f076380413ee33dd245011c953bc0a24b33f94b
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-14 12:50:11 +0300

    feat(locale): add de lang to footer  section.

--- app/[locale]/(root)/_components/footer.tsx
+++ app/[locale]/(root)/_components/footer.tsx
@@ -2,17 +2,55 @@ import Image from "next/image";
 import Link from "next/link";
 import { Facebook, Linkedin, Twitter } from "lucide-react";
 
-import { links } from "@/constants";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const ctx = useTranslations("Footer");
+
+  const links = [
+    {
+      id: 0,
+      title: ctx("title"),
+      links: [
+        { id: 0, name: ctx("links.0.name"), href: "/about-us" },
+        { id: 1, name: ctx("links.1.name"), href: "/career" },
+        { id: 2, name: ctx("links.2.name"), href: "/blog" },
+        { id: 3, name: ctx("links.3.name"), href: "/contact-us" },
+      ],
+    },
+    {
+      id: 1,
+      title: ctx("for_businesses.title"),
+      links: [
+        {
+          id: 0,
+          name: ctx("for_businesses.links.0.name"),
+          href: "/corporate-training",
+        },
+        {
+          id: 1,
+          name: ctx("for_businesses.links.1.name"),
+          href: "/digital-transformation",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: ctx("work_with_us.title"),
+      links: [
+        { id: 0, name: ctx("work_with_us.links.0.name"), href: "/blog-guest" },
+      ],
+    },
+  ];
+
   return (
     <footer className="pt-16 pb-8 bg-[#222222]">
       <div className="max-w-screen-xl mx-auto px-4 md:px-8 space-y-20">
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           <div className="flex flex-col gap-5">
-            <h3 className="text-white font-medium">Follow us</h3>
+            <h3 className="text-white font-medium">{ctx("social")}</h3>
             <div className="flex gap-4">
               <Button
                 size="icon"
