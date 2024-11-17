23c9f213f20b46e99e172745eebf42ff8186efd3
Saeed Altout <saeedaltoutpro@gmail.com>
2024-11-17 23:01:46 +0300

    fix error type

--- app/[locale]/auth/[role]/create-new-team/page.tsx
+++ app/[locale]/auth/[role]/create-new-team/page.tsx
@@ -1,14 +1,19 @@
-import { redirect } from "next/navigation";
-
-import { Role } from "@/config";
+import { Role } from "@/config/enums";
+import { redirect } from "@/i18n/routing";
 import { CreateNewTeamForm } from "@/components/auth/create-new-team-form";
 
 export default async function CreateNewTeamPage({
   params,
 }: {
-  params: Promise<{ role: string }>;
+  params: Promise<{ role: Role }>;
 }) {
   const { role } = await params;
-  if (role !== Role.BUSINESS) redirect("/auth/login");
+
+  if (!Object.values(Role).includes(role as Role)) {
+    redirect({
+      href: `/auth/${Role.STUDENT}/login`,
+      locale: "en",
+    });
+  }
   return <CreateNewTeamForm role={role} />;
 }
